80d44ac453091dfb9be067be23c79d476f1561cd
Timalexanderandersson <timalexanderandersson@hotmail.com>
2025-02-26 14:30:08 +0100

    created usestate and eventhandler for the registrtaion

--- task-builder-block/src/pages/registration.js
+++ task-builder-block/src/pages/registration.js
@@ -1,10 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "../styles/registration.module.css"
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 
 const Registrations = () => {
+
+    const [registers, setRegisters] = useState({
+        'username':"",
+        'password':"",
+        'password2':"",
+    })
+
+    const {username, password, password2} = registers;
+
+    const handleChange = (event) => {
+        setRegisters({
+            ...registers,
+            [event.target.name]: event.target.value,
+        })
+
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            
+        } catch (error) {
+            
+        }
+
+    }
 
     return (
         <div className={styles.bigreg}>
@@ -15,17 +41,32 @@ const Registrations = () => {
                 <Form>
                     <Form.Group className="mb-3" controlId="formBasicUsername">
                         <Form.Label>Username</Form.Label>
-                        <Form.Control type="text" placeholder="Enter username" />
+                        <Form.Control 
+                        type="text"
+                        name="username"
+                        value={username}
+                         placeholder="Enter username"
+                         onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter password" />
+                        <Form.Control 
+                        type="password"
+                         placeholder="Enter password"
+                         name="password"
+                         value={password}
+                         onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Confirm password" />
+                        <Form.Control 
+                        type="password"
+                         placeholder="Confirm password"
+                         name="password2"
+                         value={password2}
+                         onChange={handleChange} />
                     </Form.Group>
                     <Button  type="submit" className={styles.buttonsubmit}>
                         Register
